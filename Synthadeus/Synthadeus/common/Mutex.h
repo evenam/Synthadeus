72b241d8892e28af55fc11a9d5fae26041d8cdd8
Everett Moser <jmoser5@lsu.edu>
2015-09-16 12:02:11 -0500

    Added in URL links to MSDN to Semaphore and Mutex

--- Synthadeus/Synthadeus/common/Mutex.h
+++ Synthadeus/Synthadeus/common/Mutex.h
@@ -4,6 +4,7 @@
 #include "Object.h"
 
 // Basic mutex lock skeleton
+// see https://msdn.microsoft.com/en-us/library/windows/desktop/ms684266(v=vs.85).aspx
 
 class Mutex : public Object
 {
--- Synthadeus/Synthadeus/common/Semaphore.h
+++ Synthadeus/Synthadeus/common/Semaphore.h
@@ -4,6 +4,7 @@
 #include "Object.h"
 
 // Basic semaphore skeleton
+// see https://msdn.microsoft.com/en-us/library/windows/desktop/ms685129(v=vs.85).aspx
 
 class Semaphore : public Object
 {
