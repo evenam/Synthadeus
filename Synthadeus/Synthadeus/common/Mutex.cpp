c92546dbfe8bc1114435b6eebe0d807de0d2241e
Dan Robert <drobe504@gmail.com>
2015-09-16 22:06:47 -0500

    Implemented Mutex wrapper

--- Synthadeus/Synthadeus/common/Mutex.cpp
+++ Synthadeus/Synthadeus/common/Mutex.cpp
@@ -1,22 +1,42 @@
 #include "Mutex.h"
 
+/**
+* @brief Constructor (doesn't lock mutex by default)
+*/
 Mutex::Mutex()
 {
+	m = CreateMutex(NULL, FALSE, NULL);
 }
 
+/**
+* @brief Destructor.
+*/
 Mutex::~Mutex()
 {
+	CloseHandle(m);
 }
 
+/**
+* @brief Locks mutex.
+*/
 void Mutex::lock()
 {
+	WaitForSingleObject(m, INFINITE);
 }
 
+/**
+* @brief Unlocks mutex.
+*/
 void Mutex::unlock()
 {
+	ReleaseMutex(m);
 }
 
+/**
+* @brief Checks status of mutex. 
+*  Locks mutex if immediately available.
+*/
 bool Mutex::check()
 {
-	return false;
+	return WaitForSingleObject(m, 0) == WAIT_OBJECT_0;
 }
