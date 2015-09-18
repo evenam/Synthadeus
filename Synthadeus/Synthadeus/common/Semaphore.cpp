1bff94786ffe49c15e29c05a4173bc4abcea305a
James Bellard <jbella5@lsu.edu>
2015-09-18 00:00:30 -0500

    Added semaphore.

--- Synthadeus/Synthadeus/common/Semaphore.cpp
+++ Synthadeus/Synthadeus/common/Semaphore.cpp
@@ -1,22 +1,41 @@
 #include "Semaphore.h"
 
-Semaphore::Semaphore()
+#define lockTime 1000
+
+Semaphore::Semaphore(int numberOfThreads)
 {
+	int s = numberOfThreads;
+	//CreateSemaphore();
 }
 
 Semaphore::~Semaphore()
 {
 }
-
+//increment the semaphore value.
 void Semaphore::signal()
 {
+	s++;
 }
 
-void Semaphore::wait()
+/*if semaphore value is positive, decrement the value, otherwise
+suspend the thread and block on that semaphore until it is positive
+*/
+void Semaphore::wait(HANDLE hHandle)
 {
-}
+	if (s > 0) {
+		s--;
+	}
 
+	else while (s <= 0) {
+		//Makes the current thread wait
+		WaitForSingleObject(hHandle, lockTime);
+	}
+}
+//returns whether a thread is available to be used
 bool Semaphore::check()
 {
+	if (1 != 0) {
+		return true;
+	}
 	return false;
 }
