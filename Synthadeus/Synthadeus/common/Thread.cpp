fe5d4114dbe8c4768cfc04658da569b13e474931
Everett Moser <jmoser5@lsu.edu>
2015-09-21 15:35:00 -0500

    Added in Thread (untested) TODO: test theading interface

---
+++ Synthadeus/Synthadeus/common/Thread.cpp
@@ -0,0 +1,53 @@
+#include "Thread.h"
+
+Thread::Thread() : 
+	sharedMemory(NULL), 
+	memorySize(0), 
+	memoryOwner(false)
+{
+	initialize();
+}
+
+Thread::Thread(unsigned int sharedMemorySize) : 
+	sharedMemory(new char[sharedMemorySize]), 
+	memorySize(sharedMemorySize), 
+	memoryOwner(true)
+{
+	initialize();
+}
+
+Thread::Thread(void * const previouslyAllocatedSharedMemory) : 
+	sharedMemory(previouslyAllocatedSharedMemory), 
+	memorySize(sizeof(previouslyAllocatedSharedMemory)), 
+	memoryOwner(false)
+{
+	initialize();
+}
+
+void Thread::initialize()
+{
+	threadHandle = CreateThread(NULL, 0, threadFunc, this, 0, &threadID);
+	if (threadHandle == NULL)
+		AssertWindowsError();
+}
+
+Thread::~Thread()
+{
+	if (memoryOwner)
+	{
+		assert(sharedMemory != NULL)
+		sharedMemoryLock.lock();
+		delete[] sharedMemory;
+	}
+}
+
+DWORD WINAPI Thread::threadFunc(LPVOID lParam)
+{
+	Thread* thread = (Thread*)lParam;
+	thread->run();
+}
+
+void Thread::wait(Thread* thread)
+{
+	WaitForSingleObject(thread->threadHandle, INFINITE);
+}
---
+++ Synthadeus/Synthadeus/common/Thread.h
@@ -0,0 +1,70 @@
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+//   Thread Interface                                                         //
+//   Everett Moser                                                            //
+//   9-8-15                                                                   //
+//                                                                            //
+//   A generic thread interface as a base for Threads.                        //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <Windows.h>
+#include "Mutex.h"
+#include "Error.h"
+#include "Object.h"
+
+// bytes of memory
+#define THREAD_DEFAULT_MEMORY_SIZE 1024
+#define THREAD_NO_SHARED_MEMORY 0
+#define THREAD
+
+class Thread : public Object
+{
+private:
+	Mutex sharedMemoryLock;
+	void* const sharedMemory;
+	const bool memoryOwner;
+	const unsigned int memorySize;
+	static DWORD WINAPI threadFunc(LPVOID lParam);
+	HANDLE threadHandle;
+	DWORD threadID;
+	void initialize();
+
+public:
+	RTTI_MACRO(Thread);
+
+	Thread(); // Thread with no shared memory
+	Thread(unsigned int sharedMemorySize); // Thread with owned, newly created heap memory
+	Thread(void* const previouslyAllocatedSharedMemory); // Thread with non-owned heap memory
+	~Thread();
+
+	// get access to shared memory
+	inline void* const getSharedMemory() 
+	{
+		assert(sharedMemory != NULL); 
+		sharedMemoryLock.lock(); 
+		return sharedMemory;
+	}
+
+	template<class TYPE> inline TYPE* const getSharedMemory() 
+	{
+		assert(sharedMemory != NULL); 
+		sharedMemoryLock.lock();
+		return reinterpret_cast<TYPE*>(sharedMemory);
+	}
+
+	// release access to shared memory
+	inline void releaseSharedMemory() 
+	{
+		assert(sharedMemory != NULL); 
+		assert(!sharedMemoryLock.check()); 
+		sharedMemoryLock.unlock(); 
+	}
+
+	// abstract run and wait function
+	virtual void run() = 0;
+	static void wait(Thread* thread);
+};
+
