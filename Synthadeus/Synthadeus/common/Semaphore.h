c0dd1d19e58f09f708c42c65700c09244b9e50c8
James Bellard <jbella5@lsu.edu>
2015-09-26 13:14:52 -0500

    Did more work on semaphore

--- Synthadeus/Synthadeus/common/Semaphore.h
+++ Synthadeus/Synthadeus/common/Semaphore.h
@@ -3,6 +3,8 @@
 #include <Windows.h>
 #include "Object.h"
 
+//this is a wrapper for windows semaphore object
+
 // Basic semaphore skeleton
 // see https://msdn.microsoft.com/en-us/library/windows/desktop/ms685129(v=vs.85).aspx
 
@@ -10,15 +12,23 @@ class Semaphore : public Object
 {
 private:
 
+	HANDLE s;
+
+	//define constants
+	CONST int LockTime= 1000;
+	CONST long MaxCount =10;
+	CONST long InitCount =0;
+	//the amount to increment semaphore
+	CONST long IncCount = 1;
 
 public:
 	RTTI_MACRO(Semaphore);
 
-	Semaphore(int);
+	Semaphore(int, LPCTSTR);
 	~Semaphore();
 
 	void signal();
-	void wait(HANDLE);
+	void wait(int);
 	bool check();
 };
 
