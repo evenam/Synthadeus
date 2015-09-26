85a68a9637b2dcfbb946be61dc532b1f4a0a1689
James Bellard <jbella5@lsu.edu>
2015-09-26 13:14:52 -0500

    Did more work on semaphore

--- Synthadeus/Synthadeus/common/Semaphore.cpp
+++ Synthadeus/Synthadeus/common/Semaphore.cpp
@@ -1,41 +1,52 @@
 #include "Semaphore.h"
 
-#define lockTime 1000
-
-Semaphore::Semaphore(int numberOfThreads)
+//construct a semaphore object
+Semaphore::Semaphore(int numberOfResources, LPCTSTR lpName)
 {
-	int s = numberOfThreads;
-	//CreateSemaphore();
+	//int s = handle to semaphore object
+	s = CreateSemaphore(NULL, InitCount, MaxCount,lpName);
+	if (!s) 
+	{
+		AssertWindowsError();
+	}
 }
+
+
 
 Semaphore::~Semaphore()
 {
+	if (!CloseHandle(s))
+		AssertWindowsError();
 }
+
 //increment the semaphore value.
 void Semaphore::signal()
 {
-	s++;
+	ReleaseSemaphore(s,IncCount,NULL);
 }
 
 /*if semaphore value is positive, decrement the value, otherwise
-suspend the thread and block on that semaphore until it is positive
+suspend the thread and block on that semaphore until it is positive.
+Input: the number of resources being requested.
 */
-void Semaphore::wait(HANDLE hHandle)
+void Semaphore::wait(int numReources)
 {
 	if (s > 0) {
-		s--;
+		//this may not work with a negative value. if this doesn't work, i can try recreating semaphore.
+		ReleaseSemaphore(s, -IncCount, NULL);
 	}
 
 	else while (s <= 0) {
 		//Makes the current thread wait
-		WaitForSingleObject(hHandle, lockTime);
+		WaitForSingleObject(s, LockTime);
 	}
 }
+
 //returns whether a thread is available to be used
 bool Semaphore::check()
 {
-	if (1 != 0) {
+	//GetLastError May work.
+	
 		return true;
-	}
-	return false;
+	
 }
