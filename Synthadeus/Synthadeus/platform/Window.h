41038d617d0dbc63c428bc82a69d341bd081b883
Everett Moser <foolsboy137@gmail.com>
2015-09-28 20:38:24 -0500

    Completed Window interface

--- Synthadeus/Synthadeus/platform/Window.h
+++ Synthadeus/Synthadeus/platform/Window.h
@@ -1,13 +1,5 @@
 #pragma once
 
-#include <Windows.h>
-#include "Error.h"
-
-#ifndef HINST_THISCOMPONENT
-EXTERN_C IMAGE_DOS_HEADER __ImageBase;
-#define HINST_THISCOMPONENT ((HINSTANCE)&__ImageBase)
-#endif
-
 ////////////////////////////////////////////////////////////////////////////////
 //                                                                            //
 //   Window Something or other                                                //
@@ -17,31 +9,47 @@ EXTERN_C IMAGE_DOS_HEADER __ImageBase;
 //   Making windows and shit? IDK Cplus+				                      //
 //   static members on class                                                  //
 ////////////////////////////////////////////////////////////////////////////////
+
+#include <Windows.h>
+#include "Error.h"
+#include "resource.h"
+
+// neat little macro to extract hInstance from exe
+#ifndef HINST_THISCOMPONENT
+EXTERN_C IMAGE_DOS_HEADER __ImageBase;
+#define HINST_THISCOMPONENT ((HINSTANCE)&__ImageBase)
+#endif
+
 //Arbitrary sizes
 #define WINDOW_DEFAULT_WIDTH 640
 #define WINDOW_DEFAULT_HEIGHT 480
 
-// preliminary skeleton for a window class
-
+// abstract class to help with making a window
 class Window
 {
 private:
-	void handleMessage(UINT msg); // one cycle of the event loop, eventually we will run this on a separate thread or two
-	HWND hWnd;
+	// handles a message, -1 if message not handled
+	virtual inline int handleMessage(UINT msg, WPARAM wParam, LPARAM lParam) = 0;
 	int wndHeight, wndWidth;
+	bool isInitialized;
+
+	virtual inline bool initialize() { return true;  } // set up the window, present it
+	virtual inline bool uninitialize() { return true; } // safely free all of the resources
+
+	// windows specific statics
 	static LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 	static const char* wndClassName;
+
+	// window!
+	HWND hWnd;
 	
 public:
 
-	Window(int wndWidth = WINDOW_DEFAULT_HEIGHT, int wndHeight = WINDOW_DEFAULT_HEIGHT);
+	Window(int nCmdShow = SW_SHOWNORMAL, int wndWidth = WINDOW_DEFAULT_HEIGHT, int wndHeight = WINDOW_DEFAULT_HEIGHT);
 	~Window();
-
-	void initialize(); // set up the window, present it
-	void uninitialize(); // safely free all of the resources
 
 	void runMessageLoop(); // returns on application exit
 	
-	HWND& getWindowHandle(); //window handler
+	HWND getWindowHandle(); //window handler
 };
 
