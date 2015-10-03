4af340ec00ece58bcf86399a3cc75ad9f90406ae
James Bellard <jbella5@lsu.edu>
2015-10-03 10:07:55 -0500

    Added line class

---
+++ Synthadeus/Synthadeus/common/Line.cpp
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Class1
+{
+
+    //takes input and stores.
+	public Class1(int xStart, int yStart, int xEnd, int yEnd)
+	{
+		x0 = xStart;
+		y0 = yStart;
+		x1 = xEnd;
+		y1 = yEnd;
+	}
+}
