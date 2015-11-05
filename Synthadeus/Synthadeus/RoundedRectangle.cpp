76ee9e20ae236ab85dde4f94c6af1eb2211fbb4a
Justin <Rossj190@gmail.com>
2015-11-04 22:05:32 -0600

    Rectangle, RoundedRectangle

---
+++ Synthadeus/Synthadeus/RoundedRectangle.cpp
@@ -0,0 +1,16 @@
+#include "RoundedRectangle.h"
+
+
+
+RoundedRectangle::RoundedRectangle(float x, float y, float xRadius, float yRadius)
+{
+	this->x = x;
+	this->y = y;
+	this->xRadius = xRadius;
+	this->yRadius = yRadius;
+}
+
+
+RoundedRectangle::~RoundedRectangle()
+{
+}
