f69cf7efca1d57f53f87561a3cbd539f45056948
Everett Moser <foolsboy137@gmail.com>
2015-11-12 23:01:45 -0600

    quality of life update for main.cpp

--- Synthadeus/Synthadeus/platform/Main.cpp
+++ Synthadeus/Synthadeus/platform/Main.cpp
@@ -1,22 +1,32 @@
 #include "Error.h"
+#include "CFMaths.h"
 #include "Synthadeus.h"
 
+// program entry point
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpwCmdLine, int nCmdShow)
 {
 	// intialize subcomponents
 	DebugLogging::initDebugLogger();
+	CFMaths::init();
 
+	// set up heap
 	HeapSetInformation(NULL, HeapEnableTerminationOnCorruption, NULL, 0);
+
+	// initialize with COM
 	if (SUCCEEDED(CoInitialize(NULL)))
 	{
+		// run the application
 		Synthadeus synthesizer;
 		synthesizer.run();
+
+		// uninitialize COM
 		CoUninitialize();
 	}
 
-	// uninitialize subcomponents and 
+	// uninitialize subcomponents and make sure we have cleaned up
+	Object::AssertNoAbandonObjects();
 	DebugLogging::finishDebugLogger();
-	//Thread::assertAllThreadsHaveTerminated();
-	//Object::AssertNoAbandonObjects();
-	return 0;
+
+	// exit success!
+	return EXIT_SUCCESS;
 }
