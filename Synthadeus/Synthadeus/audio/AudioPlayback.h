0a04d8740627fa9feebd05c7fe001bf08f7b5648
Everett Moser <foolsboy137@gmail.com>
2015-11-27 16:03:55 -0600

    Test tone, please ignore

--- Synthadeus/Synthadeus/audio/AudioPlayback.h
+++ Synthadeus/Synthadeus/audio/AudioPlayback.h
@@ -20,6 +20,8 @@
 
 #pragma comment(lib, "portaudio_x86.lib")
 
+#define AUDIO_DEVICE (Pa_GetHostApiInfo(Pa_HostApiTypeIdToHostApiIndex(paASIO))->defaultOutputDevice)
+
 class AudioOutputNode;
 
 class AudioPlayback
@@ -28,16 +30,19 @@ private:
 	AudioOutputNode* node;
 	InputDevice::Piano* vPiano;
 
-	bool initialized = false;
+	PaStream* stream;
+
+	bool initialized;
 
 	// tuned for A4 to be 440 Hz
 	inline float getFrequencyForNote(int note) { return AUDIO_TUNE_FREQUENCY * fpowf(1.0594631f, (note - AUDIO_TUNE_NOTE)); };
-
-	float thetas[InputDevice::Piano::OCTAVES][InputDevice::Piano::KEYS];
+	float thetas[InputDevice::Piano::TOTAL_KEYS];
 
 public:
 	AudioPlayback(AudioOutputNode* outputNode, InputDevice::Piano* virtualPiano);
 	bool initialize();
 	bool deinitialize();
 	inline bool isInitialized() { return initialized; };
+	static int AudioCallback(const void* inputBuffer, void* outputBuffer, unsigned long framesPerBuffer, const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags, void* userdata);
+	void updateTheta(int key);
 };
