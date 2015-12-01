56db7a28ab8f1030d6fc9c3a52b843849138c2d7
Everett Moser <foolsboy137@gmail.com>
2015-11-30 21:56:03 -0600

    slider base decs

--- Synthadeus/Synthadeus/ux_comp/base/SliderBase.cpp
+++ Synthadeus/Synthadeus/ux_comp/base/SliderBase.cpp
@@ -2,26 +2,45 @@
 
 SliderBase::SliderBase(float minValue, float maxValue, float initialValue, float tickValue) : minimum(minValue), maximum(maxValue), currentValue(initialValue), tick(tickValue)
 {
+	// get the closest tick
 	int ticks = initialValue / tickValue;
+
+	// update the value to the value of the closest tick
 	currentValue = ticks * tickValue;
+
+	// restrict slider range
 	if (currentValue < minimum) currentValue = minimum;
 	if (currentValue > maximum) currentValue = maximum;
 }
 
 void SliderBase::update(float deltaValue)
 {
+	// add in delta value
 	currentValue += deltaValue;
+
+	// calculate nearest tick
 	int ticks = currentValue / tick;
+
+	// update value to the tick value
 	currentValue = ticks * tick;
+
+	// restrict by slider range
 	if (currentValue < minimum) currentValue = minimum;
 	if (currentValue > maximum) currentValue = maximum;
 }
 
 void SliderBase::setValue(float newValue)
 {
+	// update the value 
 	currentValue = newValue;
+
+	// calculate nearest tick
 	int ticks = currentValue / tick;
+
+	// update value to the tick value
 	currentValue = ticks * tick;
+
+	// restrict by slider range
 	if (currentValue < minimum) currentValue = minimum;
 	if (currentValue > maximum) currentValue = maximum;
 }
